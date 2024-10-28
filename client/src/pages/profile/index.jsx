38683dbfc5b5cf800fbc7409d25c06e1a67792de
sami-ul-haq <samiulhaq1899@gmail.com>
2024-10-28 19:36:22 +0500

    more code

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,21 +1,101 @@
 import { useAppStore } from "@/store/Index";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
+import { FaPlus, FaTrash } from "react-icons/fa";
+
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { getColor } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
 
 const Profile = () => {
   const { userInfo } = useAppStore();
+  const navigate = useNavigate();
 
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [image, setImage] = useState(null);
   const [hovered, setHovered] = useState(false);
-  const [selectedColor, setselectedColor] = useState("");
+  const [selectedColor, setselectedColor] = useState(0);
 
   console.log(userInfo);
 
+  const saveChanges = async () => {};
+
   return (
-    <div>
-      Profile
-      <h1>Email: {userInfo.email}</h1>
+    <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
+      <div className="flex flex-col gap-10 w-[80vw] md-w-max">
+        <div>
+          <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer" />
+        </div>
+        <div className="grid grid-cols-2">
+          <div
+            className="h-full w-32 md:h-48 relative flex items-center justify-center"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          >
+            <Avatar className="h-32 w-32 md:w-48 md:h-48 rounded-full overflow-hidden">
+              {image ? (
+                <AvatarImage
+                  src={image}
+                  alt="profile"
+                  className="object-cover w-full h-full bg-black"
+                />
+              ) : (
+                <div
+                  className={`uppercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[1px] flex items-center justify-center rounded-full ${getColor(
+                    selectedColor
+                  )}`}
+                >
+                  {firstName
+                    ? firstName.split("").shift()
+                    : userInfo.email.split("").shift()}
+                </div>
+              )}
+            </Avatar>
+
+            {hovered && (
+              <div className="absolute inset-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full">
+                {image ? (
+                  <FaTrash className="text-white tex-3xl cursor-pointer" />
+                ) : (
+                  <FaPlus className="text-white tex-3xl cursor-pointer" />
+                )}
+              </div>
+            )}
+            {/* <input type="text" /> */}
+          </div>
+          <div className="lex min-w-32 md:min-w-64 flex-col gap-5 text-white item-center justify-center">
+            <div className="w-full">
+              <Input
+                placeholder="Email"
+                type="email"
+                disabled
+                value={userInfo.email}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full">
+              <Input
+                placeholder="First Name"
+                type="text"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full">
+              <Input
+                placeholder="Last Name"
+                type="text"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
